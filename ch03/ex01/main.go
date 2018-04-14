5e597a6650d3689c84af906bf4085610a7b110d9
Ivan de la Beldad Fernandez <ivandelabeldad@gmail.com>
2018-04-14 21:09:01 +0200

    exercises: handle errors and skip exercise ch03/ex02

---
+++ ch03/ex01/main.go
@@ -0,0 +1,64 @@
+// If the function f returns a non-finite float64 value, the SVG file will contain
+// invalid <polygon> elements (although many SVG renderers handle this gracefully). Modify
+// the program to skip invalid polygons.
+
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+const (
+	width, height = 600, 320            // canvas size in pixels
+	cells         = 100                 // number of grid cells
+	xyrange       = 30.0                // axis ranges (-xyrange..+xyrange)
+	xyscale       = width / 2 / xyrange // pixels per x or y unit
+	zscale        = height * 0.4        // pixels per z unit
+	angle         = math.Pi / 6         // angle of x, y axes (=30°)
+)
+
+var sin30, cos30 = math.Sin(angle), math.Cos(angle) // sin(30°), cos(30°)
+
+func main() {
+	fmt.Printf("<svg xmlns='http://www.w3.org/2000/svg' "+
+		"style='stroke: grey; fill: white; stroke-width: 0.7' "+
+		"width='%d' height='%d'>", width, height)
+	for i := 0; i < cells; i++ {
+		for j := 0; j < cells; j++ {
+			ax, ay, okA := corner(i+1, j)
+			bx, by, okB := corner(i, j)
+			cx, cy, okC := corner(i, j+1)
+			dx, dy, okD := corner(i+1, j+1)
+			if !okA || !okB || !okC || !okD {
+				continue
+			}
+			fmt.Printf("<polygon points='%g,%g %g,%g %g,%g %g,%g'/>\n",
+				ax, ay, bx, by, cx, cy, dx, dy)
+		}
+	}
+	fmt.Println("</svg>")
+}
+
+func corner(i, j int) (sx float64, sy float64, ok bool) {
+	// Find point (x,y) at corner of cell (i,j).
+	x := xyrange * (float64(i)/cells - 0.5)
+	y := xyrange * (float64(j)/cells - 0.5)
+
+	// Compute surface height z.
+	z, ok := f(x, y)
+
+	// Project (x,y,z) isometrically onto 2-D SVG canvas (sx,sy).
+	sx = width/2 + (x-y)*cos30*xyscale
+	sy = height/2 + (x+y)*sin30*xyscale - z*zscale
+	return sx, sy, ok
+}
+
+func f(x, y float64) (f float64, ok bool) {
+	r := math.Hypot(x, y) // distance from (0,0)
+	f = math.Sin(r) / r
+	if math.IsNaN(f) || math.IsInf(f, 0) {
+		return 0, false
+	}
+	return math.Sin(r) / r, true
+}
