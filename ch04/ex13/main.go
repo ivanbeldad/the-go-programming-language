d2ee88739f44143ce4ad466bdf728540e1136bd6
Ivan de la Beldad Fernandez <ivandelabeldad@gmail.com>
2018-04-19 23:54:13 +0200

    exercises: add statements to incomplete exercises on chapter 04

---
+++ ch04/ex13/main.go
@@ -0,0 +1,9 @@
+// The JSON-based web ser vice of the Open Movie Database lets you search
+// https://omdbapi.com/ for a movie by name and download its poster image. Write a tool
+// poster that downloads the poster image for the movie named on the command line.
+
+package main
+
+func main() {
+
+}
---
+++ ch04/ex14/main.go
@@ -0,0 +1,8 @@
+// Create a web server that queries GitHub once and then allows navigation of the
+// list of bug reports, milestones, and users.
+
+package main
+
+func main() {
+
+}
