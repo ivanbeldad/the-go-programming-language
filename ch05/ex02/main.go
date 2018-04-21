728bc606ccd892b7b1fc7bac17fb2cfe0e5bc74a
Ivan de la Beldad Fernandez <ivandelabeldad@gmail.com>
2018-04-21 23:23:54 +0200

    exercises: recursive functions, html parsing and templating

--- ch05/ex02/main.go
+++ ch05/ex02/main.go
@@ -3,6 +3,48 @@
 
 package main
 
-func main() {
+import (
+	"fmt"
+	"io"
+	"os"
+	"text/template"
 
+	"golang.org/x/net/html"
+)
+
+// NodeCount ...
+type NodeCount map[string]int
+
+// Fprint ...
+func (nc NodeCount) Fprint(w io.Writer) (err error) {
+	tpl, err := template.ParseFiles("nodeCountTpl.txt")
+	if err != nil {
+		return
+	}
+	return tpl.Execute(w, nc)
+}
+
+// Print ...
+func (nc NodeCount) Print() error {
+	return nc.Fprint(os.Stdout)
+}
+
+func main() {
+	doc, err := html.Parse(os.Stdin)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "findlinks1: %v\n", err)
+		os.Exit(1)
+	}
+	nodeCount := NodeCount{}
+	fill(&nodeCount, doc)
+	nodeCount.Print()
+}
+
+func fill(nc *NodeCount, cn *html.Node) {
+	if cn.Type == html.ElementNode {
+		(*nc)[cn.Data]++
+	}
+	for next := cn.FirstChild; next != nil; next = next.NextSibling {
+		fill(nc, next)
+	}
 }
