96f8e90ff10e58f7df773c243826cee1f0d83fac
Ivan de la Beldad Fernandez <ivandelabeldad@gmail.com>
2018-04-22 06:40:21 +0200

    exercises: add statements for chapter 06

---
+++ ch06/ex01/main.go
@@ -0,0 +1,11 @@
+// Implement these additional methods:
+// func (*IntSet) Len() int // return the number of elements
+// func (*IntSet) Remove(x int) // remove x from the set
+// func (*IntSet) Clear() // remove all elements from the set
+// func (*IntSet) Copy() *IntSet // return a copy of the set
+
+package main
+
+func main() {
+
+}
---
+++ ch06/ex02/main.go
@@ -0,0 +1,8 @@
+// Define a variadic (*IntSet).AddAll(...int) method that allows a list of values
+// to be added, such as s.AddAll(1, 2, 3).
+
+package main
+
+func main() {
+
+}
