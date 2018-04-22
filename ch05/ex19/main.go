10dcd7c8761803494771663140a412a9c7ce0b82
Ivan de la Beldad Fernandez <ivandelabeldad@gmail.com>
2018-04-22 03:36:51 +0200

    exercises: add defer, panic and recover exercises

--- ch05/ex19/main.go
+++ ch05/ex19/main.go
@@ -3,6 +3,18 @@
 
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+func main() {
+	fmt.Printf("Result: %d\n", noReturn())
+}
+
+func noReturn() (n int) {
+	n = 10
+	defer func() {
+		recover()
+	}()
+	panic(n)
 }
