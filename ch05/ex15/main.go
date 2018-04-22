6981e1ea73b1505dabf969919130ff42ba0a0b18
Ivan de la Beldad Fernandez <ivandelabeldad@gmail.com>
2018-04-22 03:15:43 +0200

    exercises: add variadics functions

--- ch05/ex15/main.go
+++ ch05/ex15/main.go
@@ -3,6 +3,61 @@
 
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+func main() {
+	fmt.Printf("%d\n", max(1, 3, 7, 2))
+	fmt.Printf("%d\n", min(-1, 3, 7, 2))
+	fmt.Printf("%d\n", max(6))
+	fmt.Printf("%d\n", maxForce(43))
+	fmt.Printf("%d\n", maxForce(43, -32, 1000))
+	fmt.Printf("%d\n", min())
+}
+
+func max(nums ...int) (max int) {
+	if len(nums) == 0 {
+		panic("max requires at least one int")
+	}
+	max = nums[0]
+	for _, n := range nums {
+		if n > max {
+			max = n
+		}
+	}
+	return
+}
+
+func min(nums ...int) (min int) {
+	if len(nums) == 0 {
+		panic("min requires at least one int")
+	}
+	min = nums[0]
+	for _, n := range nums {
+		if n < min {
+			min = n
+		}
+	}
+	return
+}
+
+func maxForce(n int, nums ...int) (max int) {
+	max = n
+	for _, num := range nums {
+		if num > max {
+			max = num
+		}
+	}
+	return
+}
+
+func minForce(n int, nums ...int) (min int) {
+	min = n
+	for _, num := range nums {
+		if num < min {
+			min = num
+		}
+	}
+	return
 }
--- ch05/ex16/main.go
+++ ch05/ex16/main.go
@@ -2,6 +2,30 @@
 
 package main
 
-func main() {
+import (
+	"fmt"
+	"strings"
+)
 
+func main() {
+	strings := []string{
+		"first",
+		"second",
+		"third",
+		"fourth",
+		"fifth",
+	}
+	fmt.Println(join("---", strings...))
+}
+
+func join(j string, strs ...string) (s string) {
+	if len(strs) == 0 {
+		return ""
+	}
+	buf := strings.Builder{}
+	for _, str := range strs {
+		buf.WriteString(str)
+		buf.WriteString(j)
+	}
+	return buf.String()[:len(buf.String())-len(j)]
 }
